8fd456e2d2d021cad43a6de30712372cbfe296b6
Duda Dornelles <dudassdornelles@gmail.com>
2013-03-17 13:00:44 -0700

    adding datamapper

--- Gemfile
+++ Gemfile
@@ -1,6 +1,11 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
-gem 'unicorn'
 gem 'slim'
 gem 'sass'
+
+gem 'datamapper'
+gem 'dm-sqlite-adapter'
+gem 'sqlite3'
+
+gem 'unicorn'
--- dudadornellescom.rb
+++ dudadornellescom.rb
@@ -1,8 +1,15 @@
 require 'sinatra'
 require 'slim'
 require 'sass'
+require 'data_mapper'
 
 set :port, 80
+
+DB_URL = {}
+DB_URL[:dev] = "sqlite3://#{Dir.pwd}/dev.db"
+DB_URL[:prod] = "sqlite3://#{ENV["HOME"]}/prod.db"
+
+DataMapper::setup(:default, DB_URL[ENV["RACK_ENV"].to_sym])
 
 class DudaDornellesCom < Sinatra::Base
 
